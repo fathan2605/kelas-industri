e25210a209abe054a2808fd1c6d5740d96f5ff18
fathoni <fathan.hardiansyah26@gmail.com>
2025-05-07 22:50:49 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,21 @@
+let books = [
+  {
+    "id": 1746595341475,
+    "title": "Boku No Pico",
+    "category": "Fiksi",
+    "author": "Katsuhiko Takayama"
+  },
+  {
+    "id": 1746595435338,
+    "title": "OverFlow",
+    "category": "Fiksi",
+    "author": "Eeyo Kurosaki"
+  },
+  {
+    "id": 1746631894072,
+    "title": "Iribitari Gal Ni Manko Tsukawasete Morau Hanashi",
+    "category": "Fiksi",
+    "author": "Manno"
+  }
+];
+module.exports = { books };
